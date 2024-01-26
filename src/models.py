f27e2e0c90b005b0453c92133a5fe70bdcc9c499
BonilKoo <bikoo95@snu.ac.kr>
2024-01-26 22:46:41 +0900

    Add model scripts

--- src/models.py
+++ src/models.py
@@ -59,3 +59,42 @@ class Transformer(nn.Module):
         # e_outputs: (batch_size, n_genes, d_embed)
         
         return e_outputs
+
+class CustomModel(nn.Module):
+    def __init__(self, embed_file, gene_list, device,
+                 n_heads=8, d_model=512, dropout=0.1, d_ff=2048, norm_first=False, n_layers=6,
+                 aggfunc='Flatten', d_hidden1=128, d_hidden2=64, slope=0.2, d_output_dict=None):
+        super(CustomModel, self).__init__()
+
+        self.transformer = Transformer(embed_file, gene_list, device,
+                                       n_heads, d_model, dropout, d_ff, norm_first, n_layers)
+
+        self.d_embed = self.transformer.encoder.d_embed
+
+        self.aggfunc = aggfunc
+        if aggfunc == 'Flatten':
+            self.flatten = nn.Flatten(start_dim=1)
+            self.dropout = nn.Dropout(dropout)
+        else:
+            self.pooling = nn.AdaptiveAvgPool1d(1)
+
+        self.task_specific_layers = nn.ModuleList([TaskSpecificLayer(len(gene_list), self.d_embed, dropout,
+                                                                     aggfunc, d_hidden1, d_hidden2, slope,
+                                                                     d_output) for d_output in d_output_dict.values()])
+
+    def forward(self, x, mask=None):
+        # x: (batch_size, n_genes)
+
+        x = self.transformer(x, mask)
+        # x: (batch_size, n_genes, d_embed)
+
+        if self.aggfunc == 'Flatten':
+            x = self.dropout(self.flatten(x))
+            # x: (batch_size, n_genes*d_embed)
+        else:
+            x = self.pooling(x).squeeze(-1)
+        # x: (batch_size, n_genes)
+
+        outputs = [layer(x) for layer in self.task_specific_layers]
+
+        return outputs
